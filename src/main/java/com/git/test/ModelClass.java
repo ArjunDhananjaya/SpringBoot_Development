19bf47e267a3d02c62051403da7aeb3542297349
Dhananjaya-K <ArjunDhananjaya@users.noreply.github.com>
2024-05-03 10:34:51 +0530

    First Commit

---
+++ src/main/java/com/git/test/ModelClass.java
@@ -0,0 +1,14 @@
+package com.git.test;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class ModelClass {
+
+	
+	Log logger = LogFactory.getLog(ModelClass.class);
+}
